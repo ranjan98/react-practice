cbf9f114f799eb38092771c7391ec46ccd69454a
Ranjan Goyal <ranjangoyal1@gmail.com>
2021-07-17 02:47:53 +0530

    Comments explaning State in React

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,10 @@ import './Card.css';
 import { useState } from 'react';
 
 function Card(props){
+    // the useState function is defined in react, which always return an array of size 2
     const [outputText, setOutputText] = useState(props.text);
     const changeText = () => {
+        // Once this function is called, it will update the value and the Card function will be called again to re-render and show the updated value on the webpage
         setOutputText("Changed Text using State");
     }
     return (
