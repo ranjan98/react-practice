07a6baabd462b9c26dcfefa4721994ef62afb1af
Ranjan Goyal <ranjangoyal1@gmail.com>
2021-07-17 18:47:55 +0530

    Rendering List Dynamically

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ function App() {
 
   const textArr = ["This is a text", "This is another text", "This is how you send props"];
   const valArr = [20.10, 300, 40.92];
+  const items = [
+    {
+      text: textArr[0],
+      value: valArr[0]
+    },
+    {
+      text: textArr[1],
+      value: valArr[1]
+    },
+    {
+      text: textArr[2],
+      value: valArr[2]
+    }
+  ]
 
   const addCardHandler = (enteredCardData) => {
     // We can copy data this way as well
@@ -17,9 +31,7 @@ function App() {
     <div>
       <h2> Something Here </h2>
       <AddCard onAddNewCard={addCardHandler} />
-      <Card text = {textArr[0]} val={valArr[0]} />
-      <Card text = {textArr[1]} val={valArr[1]} />
-      <Card text = {textArr[2]} val={valArr[2]} />
+      {items.map(item => <Card text={item.text} val={item.value} />)}
     </div>
   );
 }
